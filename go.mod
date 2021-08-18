1f1d05cbf9c9504d301b0522dbe69c2e914e0ff0
NintySwinty <31569763+NintyS@users.noreply.github.com>
2021-08-19 00:24:24 +0200

    Duży update

---
+++ go.mod
@@ -0,0 +1,21 @@
+module test2
+
+go 1.17
+
+require (
+	github.com/faiface/beep v1.0.2 // indirect
+	github.com/gen2brain/beeep v0.0.0-20210529141713-5586760f0cc1 // indirect
+	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
+	github.com/godbus/dbus/v5 v5.0.4 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20210803090616-8f023c250c89 // indirect
+	github.com/gopherjs/gopherwasm v1.1.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.2 // indirect
+	github.com/mattn/go-sqlite3 v1.14.5 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	gorm.io/driver/sqlite v1.1.4 // indirect
+	gorm.io/gorm v1.21.13 // indirect
+)
--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"io/ioutil"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 // Szablon do bazy danych
@@ -17,6 +20,21 @@ type User struct {
 	Password string
 }
 
+type Websites struct {
+	ID int
+	Url string
+}
+
+type Website struct {
+	ID int
+	Service bool
+	Rang int
+	Days int
+	SSL bool
+	Up int
+	Response float64
+	Down int
+}
 // Zabezpieczenia
 
 func Hash(password string) (string, error) {
@@ -43,7 +61,7 @@ func login(w http.ResponseWriter, req *http.Request)  {
 
 	db, err := gorm.Open(sqlite.Open("db.db"), &gorm.Config{})
 	if err != nil {
-		fmt.Fprintf(w, "Can not connect to database, sorry form problems. Please contact with administrator of this website")
+		fmt.Fprintf(w, "Cannot connect to database, sorry form problems. Please contact with administrator of this website")
 	}
 
 	result := db.Table("users").Where("name = ?", name).Find(&User{})
@@ -54,14 +72,29 @@ func login(w http.ResponseWriter, req *http.Request)  {
 
 	if !CheckHash(password, item.Password) { // Jeżeli hasła NIE są spójne
 		fmt.Println("Bad password")
-		return
+		http.Redirect(w, req, "/", http.StatusSeeOther)
 	}
-
-	fmt.Fprintf(w, "Logged!")
 
 	db = nil
 
-	http.Redirect(w, req, "https://www.google.pl", http.StatusSeeOther) // Przeniesienie na goole.pl
+	// Zmienna z adresem ID danego użytkownika
+	id := item.ID
+
+	// Tworzenie ciasteczka z danymi
+	c := &http.Cookie {
+		Name: "id",
+		Value:  strconv.Itoa(id),
+		HttpOnly: true,
+	}
+
+	// Ustawianie odpowiadacza dla ciastka
+	http.SetCookie(w, c)
+
+	// Utworzenie ciastka (dodanie go)
+	req.AddCookie(c)
+
+	// Przekierowywanie
+	http.Redirect(w, req, "/dashboard", http.StatusSeeOther)
 
 }
 
@@ -89,18 +122,148 @@ func register(w http.ResponseWriter, req *http.Request)  {
 
 	var item User
 
-	result = db.Table("users").Last(&item) // pobieranie odstaniniego rekordu ( najwyższego ID )
+	result = db.Table("users").Last(&item) // pobieranie ostatniego rekordu ( najwyższego ID )
 
 	pass, _ := Hash(password) // Haszowanie hasła
-
 	db.Table("users").Select("ID", "Name", "Email", "Password").Create(&User{ID: item.ID+1 /* dodawanie 1 do najwyższego ID w bezie danych */, Name: name, Email: email, Password: pass})
 
 	db = nil
 
+	http.Redirect(w, req, "/", http.StatusSeeOther)
+
 }
 
-func monitor(w http.ResponseWriter, req *http.Request)  {
-	fmt.Fprintf(w, "Just Monitor :)") // Or Monika ;)
+// Site
+
+func dashboard(w http.ResponseWriter, req *http.Request) {
+
+	// Zczytywanie ciastka o nazwie "id"
+	cookie, err := req.Cookie("id")
+	if err != nil {
+		fmt.Println("er")
+	}
+
+	// Sprawdzanie wartości cookie - ciastka
+	if cookie.Value != "" {
+		fmt.Printf(cookie.Value)
+	} else {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+	}
+
+	c := &http.Cookie {
+		Name: "id",
+		MaxAge: -1,
+		HttpOnly: true,
+	}
+
+	http.SetCookie(w, c)
+
+	req.AddCookie(c)
+
+	dat, err := ioutil.ReadFile("dashboard-analytics.html")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	x := strings.Index(string(dat), "<!-- specjalny komentarz -->");
+	y := strings.Index(string(dat), "^");
+
+	fmt.Println(y)
+
+	var s string;
+	db, err := gorm.Open(sqlite.Open("db.db"), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Jest blad i chuj");
+	}
+
+	result := db.Table("users").Where("ID = ?", cookie.Value).Find(&User{})
+	var item User
+
+	result.First(&item) // pierwszy item który spełnia wymagania ( name = ? )
+	name := item.Name
+
+	var records []Websites
+	db.Table(name + "_websites").Find(&records)
+
+	for n, i := range string(dat) {
+		s += string(i);
+		if n == x+28 {
+			for _, x := range records {
+				s += "<li class=\"list-item\">"+ x.Url +"</li>"
+			}
+		}
+		// Kiedy n bedzie na poziomie value w form'ie
+		if n == y-1 {
+			s += string(name)
+		}
+	}
+
+	//s = strings.ReplaceAll(s, "0x1c", "value.push("");value.push(\"\");value.push(\"\");value.push(\"\");value.push(\"\");value.push(\"\");value.push(\"\");")
+
+	fmt.Fprintf(w, string(s))
+}
+
+func addwebsite(w http.ResponseWriter, req *http.Request) {
+
+	if err := req.ParseForm(); err != nil { // Parsowanie inputów
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	websitename := req.FormValue("addwebsite")
+	username := req.FormValue("name")
+	var masno string;
+
+	// Usuwanie ostatniej litery z nazwy (masno) gdzie masno to jest nazwa usera ukrócona o 1 literę
+	for nr, obj := range username {
+		if nr < (len(username) - 2) {
+			masno += string(obj);
+		}
+	}
+
+	const (
+		end = "_38_"
+		dot = "_46_"
+		slash = "_47_"
+		doubledot = "_58_"
+		questionMark = "_63_"
+		equal = "_61_"
+		minus = "_45_"
+		plus = "_43_"
+	)
+
+	var url string = websitename
+
+	url = strings.ReplaceAll(url, ".", dot)
+	url = strings.ReplaceAll(url, "&", end)
+	url = strings.ReplaceAll(url, "/", slash)
+	url = strings.ReplaceAll(url, ":", doubledot)
+	url = strings.ReplaceAll(url, "?", questionMark)
+	url = strings.ReplaceAll(url, "=", equal)
+	url = strings.ReplaceAll(url, "-", minus)
+	url = strings.ReplaceAll(url, "+", plus)
+
+	fmt.Println(url)
+
+	db, err := gorm.Open(sqlite.Open("db.db" ), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Wyjebalo bleda spierdalaj")
+	}
+
+	db.Migrator().CreateTable(masno + "_website_" + url)
+	db.Migrator().AddColumn(&Website{}, "oj tak tak")
+	pola := []string{" "}
+
+	for nr := 0; nr < 8; nr++ {
+		db.Migrator().AddColumn(&Website{}, "oj tak tak")
+	}
+
+	db.Table(masno + "_website_" + url).Select("ID", "Service", "Rang", "Days", "SSL", "Up", "Response", "Down").Create(&Website{ID: 1, Service: true, Rang: 2, Days: 7, SSL: true, Up: 2, Response: 7.35312321, Down: 3})
+	fmt.Println("Wysłano")
+	http.Redirect(w, req, "/dashboard", http.StatusSeeOther)
+
+	//cos := beeep.Notify("Page status", "Page status is up", "");
 }
 
 // Główny serwer
@@ -108,8 +271,10 @@ func monitor(w http.ResponseWriter, req *http.Request)  {
 func main() {
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/register", register)
-	http.HandleFunc("/monitor", monitor)
-	
+	http.HandleFunc("/dashboard", dashboard) // dashboard-analytics.html
+	http.HandleFunc("/addwebsite", addwebsite)
+	http.Handle("/", http.FileServer(http.Dir("C:/Users/Ninja/go/src/test2/")))
+
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("Nie udało się uruchomić")
